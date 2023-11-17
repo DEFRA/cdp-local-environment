210dda0d79e7c09a0f2c22aaadf5597157fcb984
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2023-11-17 13:05:57 +0000

    updates to work with portal stubs

--- scripts/portal/setup-mongo.sh
+++ scripts/portal/setup-mongo.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-mongorestore --uri=mongodb://localhost:27017 --gzip --archive=/scripts/portal.archive --drop
+#mongorestore --uri=mongodb://localhost:27017 --gzip --archive=/scripts/portal.archive --drop
+mongorestore --uri=mongodb://localhost:27017 --gzip --archive=/scripts/cdp-user-service-backend.archive --drop
--- scripts/portal/setup-queues.sh
+++ scripts/portal/setup-queues.sh
@@ -2,6 +2,7 @@
 echo Setting up Portal Queues
 aws --endpoint $LOCALSTACK_URL sqs create-queue --queue-name ecs-deployments
 aws --endpoint $LOCALSTACK_URL sqs create-queue --queue-name ecr-push-events
+aws --endpoint $LOCALSTACK_URL sqs create-queue --queue-name github-events
 echo Done!
 aws --endpoint $LOCALSTACK_URL sqs list-queues
 
