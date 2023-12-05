3cc861e0220d5bf9b7d7aec8bf830218b6cf1a75
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2023-12-05 09:27:58 +0000

    adds healthchecks, better setup

--- scripts/portal/setup-mongo.sh
+++ scripts/portal/setup-mongo.sh
@@ -2,3 +2,4 @@
 
 #mongorestore --uri=mongodb://localhost:27017 --gzip --archive=/scripts/portal.archive --drop
 mongorestore --uri=mongodb://localhost:27017 --gzip --archive=/scripts/cdp-user-service-backend.archive --drop
+
