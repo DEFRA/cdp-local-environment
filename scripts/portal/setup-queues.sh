a3dfc43393d1454b8ecd8e574d559cd03570ac35
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2024-02-05 16:07:47 +0000

    adds test run topic

--- scripts/portal/setup-queues.sh
+++ scripts/portal/setup-queues.sh
@@ -4,10 +4,14 @@ aws --endpoint $LOCALSTACK_URL sqs create-queue --queue-name ecs-deployments
 aws --endpoint $LOCALSTACK_URL sqs create-queue --queue-name ecr-push-events
 aws --endpoint $LOCALSTACK_URL sqs create-queue --queue-name github-events
 
-# stub-deployments is used only by cdp-portal-stubs to simulate deployments
 aws --endpoint $LOCALSTACK_URL sqs create-queue --queue-name deployments-from-portal
 aws --endpoint $LOCALSTACK_URL sns create-topic --name deploy-topic
 aws --endpoint $LOCALSTACK_URL sns subscribe --topic-arn arn:aws:sns:$AWS_REGION:000000000000:deploy-topic --protocol sqs --notification-endpoint  arn:aws:sqs:eu-west-2:000000000000:deployments-from-portal
+
+# stub test suite runs
+aws --endpoint $LOCALSTACK_URL sqs create-queue --queue-name run-test-from-portal
+aws --endpoint $LOCALSTACK_URL sns create-topic --name run-test-topic
+aws --endpoint $LOCALSTACK_URL sns subscribe --topic-arn arn:aws:sns:$AWS_REGION:000000000000:run-test-topic --protocol sqs --notification-endpoint  arn:aws:sqs:eu-west-2:000000000000:run-test-from-portal
 
 echo Done!
 aws --endpoint $LOCALSTACK_URL sqs list-queues
